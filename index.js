a5d7bee6b1e5b5e63cf01e6afd411686c85797f0
Apubra <apurbadaria8@gmail.com>
2021-04-05 15:32:16 +0600

    update code

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-var cron = require('node-cron');
+var cron = require("node-cron");
 
-var task = cron.schedule('* * * * *', () =>  {
-  console.log('will not execute anymore, nor be able to restart');
-  StopTask();
+var task = cron.schedule("* * * * * *", () => {
+    console.log("will not execute anymore, nor be able to restart");
+    // StopTask();
 });
+// task.destroy();
 
-let StopTask = () =>{
-    task.destroy();
+let i = 100;
+if (i == 100) {
+  task.stop();
+  // task.destroy();
 }
+
+// let StopTask = () => {
+//     console.log("Working...");
+//     task.destroy();
+// };
